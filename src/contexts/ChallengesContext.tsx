0e4947ce0764c7819ef10e3863ef6f2646db992c
felipeghidini <lipeghidini18@gmail.com>
2021-03-01 10:09:03 -0300

    Melhorando a usabilidade

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,11 +1,22 @@
 import { createContext, useState, ReactNode } from 'react';
+import challenges from '../../challenges.json';
+
+
+interface Challenge {
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
 
 interface ChallengesContextData {
     level: number;
     currentExperience: number; 
     challengesCompleted: number;
+    experienceToNextLevel: number;
+    activeChallenge: Challenge; 
     levelUp: () => void;
     startNewChallenge: () => void;
+    resetChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -17,15 +28,26 @@ export const ChallengesContext = createContext({} as ChallengesContextData);
 
 export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(0);
+    const [currentExperience, setCurrentExperience] = useState(30);
     const [challengesCompleted, setChallengesCompletade] = useState(0);
+
+    const [activeChallenge, setActiveChallenge] = useState(null);
+
+    const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
     function levelUp() {
         setLevel(level + 1);
     }
 
     function startNewChallenge() {
-        console.log('New challenge');
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
+        const challenge = challenges[randomChallengeIndex];
+
+        setActiveChallenge(challenge)        
+    }
+
+    function resetChallenge() {
+        setActiveChallenge(null);
     }
 
     return (
@@ -34,8 +56,11 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
             level,
             currentExperience, 
             challengesCompleted, 
+            experienceToNextLevel,
             levelUp,
-            startNewChallenge 
+            startNewChallenge,
+            activeChallenge,
+            resetChallenge 
             }}
         >
             {children}
